4f8c83abb75ee56721a95b5f8b0e3804f8693c7a
yarmdy <jxy2000121@sina.com>
2024-06-22 23:38:43 +0800

    添加项目文件。

---
+++ HttpProxy/Applications/HttpProxyApplication.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProxy.Applications
+{
+    public class HttpProxyApplication
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ServiceCollection _serviceCollection;
+        public IServiceProvider Services => _serviceProvider;
+
+        public static HttpProxyApplicationBuilder CreateBuilder(string[]? args = null)
+        {
+            return new(args);
+        }
+        internal HttpProxyApplication(ServiceCollection services) { 
+            _serviceCollection = services;
+            _serviceProvider = services.BuildServiceProvider();
+        }
+        public void Run()
+        {
+
+        }
+    }
+}
---
+++ HttpProxy/Applications/HttpProxyApplicationBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProxy.Applications
+{
+    public class HttpProxyApplicationBuilder
+    {
+        internal HttpProxyApplicationBuilder(string[]? args) {
+            var argslist = args?.ToList() ?? [];
+        }
+        public ServiceCollection Services { get;}= new ServiceCollection();
+        public HttpProxyApplication Build()
+        {
+            return new HttpProxyApplication(Services);
+        }
+    }
+}
